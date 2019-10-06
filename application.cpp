29e15b996a829dbb2ec86eac504a7ee09ab2a132
James Fowkes <jamesfowkes@gmail.com>
2019-10-06 22:52:23 +0100

    Update to latest RAAT

--- application.cpp
+++ application.cpp
@@ -12,7 +12,7 @@
 
 #include "http-get-server.hpp"
 
-static HTTPGetServer s_server(true);
+static HTTPGetServer s_server(NULL);
 static const raat_devices_struct * s_pDevices = NULL;
 
 static bool s_crate_is_closed = false;
@@ -30,8 +30,9 @@ static void send_standard_erm_response()
     s_server.finish_headers();
 }
 
-static void open_crate(char const * const url)
+static void open_crate(char const * const url, char const * const end)
 {
+    (void)end;
     bool ok_to_open = s_pDevices->pLockSense->state() == false;
 
     if (url)
@@ -56,8 +57,9 @@ static void open_crate(char const * const url)
     }
 }
 
-static void close_crate(char const * const url)
+static void close_crate(char const * const url, char const * const end)
 {
+    (void)end;
     bool ok_to_close = s_pDevices->pLockSense->state() == false;
 
     if (ok_to_close)
@@ -82,8 +84,9 @@ static void close_crate(char const * const url)
     }
 }
 
-static void unlock_drawer(char const * const url)
+static void unlock_drawer(char const * const url, char const * const end)
 {
+    (void)end;
     raat_logln_P(LOG_APP, PSTR("Unlocking drawer"));
     if (url)
     {
@@ -92,8 +95,9 @@ static void unlock_drawer(char const * const url)
     s_pDevices->pRelay3->set(false);
 }
 
-static void lock_drawer(char const * const url)
+static void lock_drawer(char const * const url, char const * const end)
 {
+    (void)end;
     raat_logln_P(LOG_APP, PSTR("Locking drawer"));
     if (url)
     {
@@ -102,8 +106,9 @@ static void lock_drawer(char const * const url)
     s_pDevices->pRelay3->set(true);
 }
 
-static void set_spare(char const * const url)
+static void set_spare(char const * const url, char const * const end)
 {
+    (void)end;
     raat_logln_P(LOG_APP, PSTR("Setting spare relay"));
     if (url)
     {
@@ -112,8 +117,9 @@ static void set_spare(char const * const url)
     s_pDevices->pRelay4->set(true);
 }
 
-static void clear_spare(char const * const url)
+static void clear_spare(char const * const url, char const * const end)
 {
+    (void)end;
     raat_logln_P(LOG_APP, PSTR("Clearing spare relay"));
     if (url)
     {
@@ -122,8 +128,9 @@ static void clear_spare(char const * const url)
     s_pDevices->pRelay4->set(false);
 }
 
-static void get_reset_state(char const * const url)
+static void get_reset_state(char const * const url, char const * const end)
 {
+    (void)end;
     bool b_reset_is_pressed = s_pDevices->pResetSense->state() == false;
 
     raat_logln_P(LOG_APP, PSTR("Reset state: %s"), b_reset_is_pressed ? "P" : "NP");
@@ -134,14 +141,27 @@ static void get_reset_state(char const * const url)
     s_server.add_body_P(b_reset_is_pressed ? PSTR("PRESSED\r\n\r\n") : PSTR("NOT PRESSED\r\n\r\n"));
 }
 
+static void get_lock_state(char const * const url, char const * const end)
+{
+    (void)end;
+    bool b_locked = s_pDevices->pLockSense->state() == true;
+
+    raat_logln_P(LOG_APP, PSTR("Lock state: %s"), b_locked ? "L" : "UL");
+    if (url)
+    {
+        send_standard_erm_response();
+    }
+    s_server.add_body_P(b_locked ? PSTR("LOCKED\r\n\r\n") : PSTR("UNLOCKED\r\n\r\n"));
+}
+
 static const char OPEN_CRATE_URL[] PROGMEM = "/crate/open";
 static const char CLOSE_CRATE_URL[] PROGMEM = "/crate/close";
 static const char UNLOCK_DRAWER_URL[] PROGMEM = "/drawer/unlock";
 static const char LOCK_DRAWER_URL[] PROGMEM = "/drawer/lock";
-static const char PIGEON_RELEASE_URL[] PROGMEM = "/pigeon/release";
 static const char SET_SPARE_URL[] PROGMEM = "/spare/set";
 static const char CLEAR_SPARE_URL[] PROGMEM = "/spare/clear";
 static const char RESET_GET_URL[] PROGMEM = "/reset/get";
+static const char LOCK_GET_URL[] PROGMEM = "/lock/get";
 
 static http_get_handler s_handlers[] = 
 {
@@ -152,6 +172,7 @@ static http_get_handler s_handlers[] =
     {SET_SPARE_URL, set_spare},
     {CLEAR_SPARE_URL, clear_spare},
     {RESET_GET_URL, get_reset_state},
+    {LOCK_GET_URL, get_lock_state},
     {"", NULL}
 };
 
@@ -187,7 +208,7 @@ void raat_custom_loop(const raat_devices_struct& devices, const raat_params_stru
         if (devices.pLockSense->check_low_and_clear())
         {
             raat_logln_P(LOG_APP, PSTR("Opening crate (button)"));
-            open_crate(NULL);
+            open_crate(NULL, NULL);
         }
     }
 
@@ -198,12 +219,12 @@ void raat_custom_loop(const raat_devices_struct& devices, const raat_params_stru
         if (s_crate_is_closed)
         {
             raat_logln_P(LOG_APP, PSTR("Opening crate (override)"));
-            open_crate(NULL);
+            open_crate(NULL, NULL);
         }
         else
         {
             raat_logln_P(LOG_APP, PSTR("Closing crate (override)"));
-            close_crate(NULL);   
+            close_crate(NULL, NULL);   
         }
     }
 
